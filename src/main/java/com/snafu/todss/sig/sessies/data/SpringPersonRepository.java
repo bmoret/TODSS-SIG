b65a1d20d676d0d5ece75c671ee66a77a043cd53
Bas Moret <bas.moret@student.hu.nl>
2021-03-25 14:31:44 +0100

    setup SpringPersonRepository

---
+++ src/main/java/com/snafu/todss/sig/sessies/data/SpringPersonRepository.java
@@ -0,0 +1,11 @@
+package com.snafu.todss.sig.sessies.data;
+
+
+import com.snafu.todss.sig.sessies.domain.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringPersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByEmail(String email);
+}
