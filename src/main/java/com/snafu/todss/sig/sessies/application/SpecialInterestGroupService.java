c3cdfa6860c46f65d071b967778ca40ccb3cc585
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-27 20:39:53 +0100

    Implement manager and organizers of SIG should encapsulate

--- src/main/java/com/snafu/todss/sig/sessies/application/SpecialInterestGroupService.java
+++ src/main/java/com/snafu/todss/sig/sessies/application/SpecialInterestGroupService.java
@@ -2,41 +2,55 @@ package com.snafu.todss.sig.sessies.application;
 
 import com.snafu.todss.sig.sessies.data.SpecialInterestGroupRepository;
 import com.snafu.todss.sig.sessies.domain.SpecialInterestGroup;
+import com.snafu.todss.sig.sessies.domain.person.Person;
 import com.snafu.todss.sig.sessies.presentation.dto.request.SpecialInterestGroupRequest;
 import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class SpecialInterestGroupService {
-    private final SpecialInterestGroupRepository specialInterestGroupRepository;
+    private final SpecialInterestGroupRepository SIG_REPOSITORY;
+    private final PersonService PERSON_SERVICE;
 
-    public SpecialInterestGroupService(SpecialInterestGroupRepository specialInterestGroupRepository) {
-        this.specialInterestGroupRepository = specialInterestGroupRepository;
+    public SpecialInterestGroupService(SpecialInterestGroupRepository sigRepository, PersonService personService) {
+        this.SIG_REPOSITORY = sigRepository;
+        this.PERSON_SERVICE = personService;
     }
 
     public List<SpecialInterestGroup> getAllSpecialInterestGroups() {
-        return this.specialInterestGroupRepository.findAll();
+        return this.SIG_REPOSITORY.findAll();
     }
 
     public SpecialInterestGroup getSpecialInterestGroupById(UUID id) throws NotFoundException {
-        return this.specialInterestGroupRepository.findById(id)
+        return this.SIG_REPOSITORY.findById(id)
                 .orElseThrow(() -> new NotFoundException("No special interest group with given id"));
     }
 
-    public SpecialInterestGroup createSpecialInterestGroup(SpecialInterestGroupRequest specialInterestGroupRequest) {
-        SpecialInterestGroup specialInterestGroup = new SpecialInterestGroup(specialInterestGroupRequest.subject);
-        return specialInterestGroupRepository.save(specialInterestGroup);
+    public SpecialInterestGroup createSpecialInterestGroup(SpecialInterestGroupRequest sigRequest) throws NotFoundException {
+        Person manager = this.PERSON_SERVICE.getPerson(sigRequest.managerId);
+        List<Person> organizers = getOrganizersWithIds(sigRequest.organizerIds);
+        SpecialInterestGroup specialInterestGroup = new SpecialInterestGroup(sigRequest.subject, manager, organizers, new ArrayList<>());
+        return SIG_REPOSITORY.save(specialInterestGroup);
+    }
+
+    private List<Person> getOrganizersWithIds (List<Long> organizerIds) throws NotFoundException {
+        List<Person> organizers = new ArrayList<>();
+        for (Long id : organizerIds) {
+            organizers.add(PERSON_SERVICE.getPerson(id));
+        }
+        return organizers;
     }
 
     public SpecialInterestGroup updateSpecialInterestGroup(UUID id, SpecialInterestGroupRequest specialInterestGroupRequest) throws NotFoundException {
         SpecialInterestGroup specialInterestGroup = getSpecialInterestGroupById(id);
         specialInterestGroup.setSubject(specialInterestGroupRequest.subject);
-        return this.specialInterestGroupRepository.save(specialInterestGroup);
+        return this.SIG_REPOSITORY.save(specialInterestGroup);
     }
 
     public void deleteSpecialInterestGroup(UUID id) {
-        this.specialInterestGroupRepository.deleteById(id);
+        this.SIG_REPOSITORY.deleteById(id);
     }}
--- src/main/java/com/snafu/todss/sig/sessies/presentation/dto/request/SpecialInterestGroupRequest.java
+++ src/main/java/com/snafu/todss/sig/sessies/presentation/dto/request/SpecialInterestGroupRequest.java
@@ -1,12 +1,17 @@
 package com.snafu.todss.sig.sessies.presentation.dto.request;
 
+import com.snafu.todss.sig.sessies.domain.person.Person;
 import com.sun.istack.NotNull;
+
+import java.util.List;
 
 public class SpecialInterestGroupRequest {
     @NotNull
     public String subject;
-//    @NotNull
-//    public Person manager;
-//    @NotNull
-//    public List<Person> organizers;
+
+    @NotNull
+    public Long managerId;
+
+    @NotNull
+    public List<Long> organizerIds;
 }
