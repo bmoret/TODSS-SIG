f4c2faf81089e63aabe4aa449dfd5947eeeeecb1
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-27 19:35:57 +0100

    Refacter session response dto to private finals

--- src/main/java/com/snafu/todss/sig/sessies/presentation/dto/response/SessionResponse.java
+++ src/main/java/com/snafu/todss/sig/sessies/presentation/dto/response/SessionResponse.java
@@ -6,7 +6,25 @@ import com.snafu.todss.sig.sessies.domain.session.SessionState;
 import java.util.UUID;
 
 public class SessionResponse {
-    public UUID id;
-    public SessionState state;
-    public SessionDetails details;
+    private final UUID id;
+    private final SessionState state;
+    private final SessionDetails details;
+
+    public SessionResponse(UUID id, SessionState state, SessionDetails details) {
+        this.id = id;
+        this.state = state;
+        this.details = details;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public SessionState getState() {
+        return state;
+    }
+
+    public SessionDetails getDetails() {
+        return details;
+    }
 }
