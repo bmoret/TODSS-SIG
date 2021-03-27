9269060ae9d8010cb8a45a26324278ee756843ef
Annemae <a.b.a.vandehoef@gmail.com>
2021-03-27 10:59:15 +0100

    added SpecialInterestGroup class

---
+++ src/main/java/com/snafu/todss/sig/sessies/domain/SpecialInterestGroup.java
@@ -0,0 +1,35 @@
+package com.snafu.todss.sig.sessies.domain;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "sig")
+public class SpecialInterestGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column
+    private String subject;
+
+    //private Person manager;
+
+    //private List<Person> organizers;
+
+    //private List<Session> sessions;
+
+    public SpecialInterestGroup() {}
+    public SpecialInterestGroup(
+            String subject
+    ) {
+        this.subject = subject;
+        //this.manager = manager;
+        //this.organizers = organizers;
+        //this.sessions = sessions;
+    }
+
+    //public Session addSession(Session session) {this.sessions.add(session);}
+
+    //public Session removeSession(UUID id) {this.sessions.stream().remove(where id = id);} <-- klopt nog niet
+}
