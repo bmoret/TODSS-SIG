eb9dfd0019d7ca69381570216373d73e753d1c5a
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-29 20:31:02 +0200

    Update session state

--- src/main/java/com/snafu/todss/sig/sessies/domain/session/SessionState.java
+++ src/main/java/com/snafu/todss/sig/sessies/domain/session/SessionState.java
@@ -1,6 +1,18 @@
 package com.snafu.todss.sig.sessies.domain.session;
 
 public enum SessionState {
+    DRAFT {
+        @Override
+        public SessionState next() {
+            return TO_BE_PLANNED;
+        }
+    },
+    TO_BE_PLANNED {
+        @Override
+        public SessionState next() {
+            return PLANNED;
+        }
+    },
     PLANNED {
         @Override
         public SessionState next() {
