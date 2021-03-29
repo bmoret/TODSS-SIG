b298094f9f094163b597a1646a7da536c75c4794
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-29 17:56:14 +0200

    Uncoment and implement Attendance

--- src/main/java/com/snafu/todss/sig/sessies/domain/Attendance.java
+++ src/main/java/com/snafu/todss/sig/sessies/domain/Attendance.java
@@ -1,68 +1,77 @@
 package com.snafu.todss.sig.sessies.domain;
 
+import com.snafu.todss.sig.sessies.domain.person.Person;
+import com.snafu.todss.sig.sessies.domain.session.Session;
+
 import javax.persistence.*;
-import java.util.Objects;
 import java.util.UUID;
 
+@Entity
+@Table(name = "attendance")
 public class Attendance {
     @Id
     @GeneratedValue
-    private UUID attendanceId;
+    private UUID id;
 
-    private boolean confirmed;
-    private boolean absence;
-    private boolean speaker;
+    //todo: isConfirmed en isAbsent vervangen met isPresent
+    // (aanmelden is attendance object aanmaken, komen is isPresent = true absent is false)
+    private boolean isConfirmed;
+    private boolean isAbsent;
+    private boolean isSpeaker;
 
-//    @ManyToOne
-//    @JoinColumn(name = "person_attendance", nullable = false)
-//    private Person person;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "session_attendances_list", nullable = false)
-//    private Session session;
+    @ManyToOne(optional = false)
+    private Person person;
 
+    @ManyToOne(optional = false)
+    private Session session;
+
+    public static Attendance of(Person person, Session session) {
+        return new Attendance(false, false, false, person, session);
+    }
 
     public Attendance() {}
 
-    public Attendance(boolean confirmed, boolean absence, boolean speaker) {
-        this.confirmed = confirmed;
-        this.absence = absence;
-        this.speaker = speaker;
+    public Attendance(boolean isConfirmed, boolean isAbsent, boolean speaker, Person person, Session session) {
+        this.isConfirmed = isConfirmed;
+        this.isAbsent = isAbsent;
+        this.isSpeaker = speaker;
+        this.person = person;
+        this.session = session;
     }
 
     public boolean isConfirmed() {
-        return confirmed;
+        return isConfirmed;
     }
 
-    public boolean isAbsence() {
-        return absence;
+    public boolean isAbsent() {
+        return isAbsent;
     }
 
     public boolean isSpeaker() {
-        return speaker;
+        return isSpeaker;
     }
 
     public UUID getAttendanceId() {
-        return attendanceId;
+        return this.id;
     }
 
-//    public Person getPerson() {
-//        return person;
-//    }
-//
-//    public Session getSession() {
-//        return session;
-//    }
+    public Person getPerson() {
+        return person;
+    }
+
+    public Session getSession() {
+        return session;
+    }
 
     public void setConfirmed(boolean confirmed) {
-        this.confirmed = confirmed;
+        this.isConfirmed = confirmed;
     }
 
-    public void setAbsence(boolean absence) {
-        this.absence = absence;
+    public void setAbsent(boolean absent) {
+        isAbsent = absent;
     }
 
     public void setSpeaker(boolean speaker) {
-        this.speaker = speaker;
+        isSpeaker = speaker;
     }
 }
