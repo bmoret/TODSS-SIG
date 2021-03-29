c405bed524eaadded88f3552fbbc7cbd9448b348
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-29 19:54:34 +0200

    Add @Repository Annotations

--- src/main/java/com/snafu/todss/sig/sessies/data/SessionRepository.java
+++ src/main/java/com/snafu/todss/sig/sessies/data/SessionRepository.java
@@ -2,8 +2,10 @@ package com.snafu.todss.sig.sessies.data;
 
 import com.snafu.todss.sig.sessies.domain.session.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface SessionRepository extends JpaRepository<Session, UUID> {
 }
--- src/main/java/com/snafu/todss/sig/sessies/data/SpringPersonRepository.java
+++ src/main/java/com/snafu/todss/sig/sessies/data/SpringPersonRepository.java
@@ -3,10 +3,12 @@ package com.snafu.todss.sig.sessies.data;
 
 import com.snafu.todss.sig.sessies.domain.person.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface SpringPersonRepository extends JpaRepository<Person, UUID> {
     Optional<Person> findByEmail(String email);
 }
