72014e3539dcc5530590f1d2119dcf53692b7ace
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-04-04 20:00:14 +0200

    Add next state test

--- src/test/java/com/snafu/todss/sig/sessies/domain/session/types/PhysicalSessionTest.java
+++ src/test/java/com/snafu/todss/sig/sessies/domain/session/types/PhysicalSessionTest.java
@@ -6,10 +6,13 @@ import com.snafu.todss.sig.sessies.domain.session.SessionState;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -47,5 +50,37 @@ class PhysicalSessionTest {
         assertEquals(description, details.getDescription());
         assertEquals(state, session.getState());
         assertEquals(address, session.getAddress());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideSessionStates")
+    @DisplayName("Go to next session state and expect session state")
+    void goToNextSession(SessionState state, SessionState expectedNextState) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime nowPlusOneHour = LocalDateTime.now().plusHours(1);
+        String subject = "Subject";
+        String description = "Description";
+        String address = "Address";
+        session = new PhysicalSession(
+                new SessionDetails(now, nowPlusOneHour, subject, description),
+                state,
+                new SpecialInterestGroup(),
+                new ArrayList<>(),
+                new ArrayList<>(),
+                address
+        );
+
+        session.nextState();
+
+        assertEquals(expectedNextState, session.getState());
+    }
+    static Stream<Arguments> provideSessionStates() {
+        return Stream.of(
+                Arguments.of(SessionState.DRAFT, SessionState.TO_BE_PLANNED),
+                Arguments.of(SessionState.TO_BE_PLANNED, SessionState.PLANNED),
+                Arguments.of(SessionState.PLANNED, SessionState.ONGOING),
+                Arguments.of(SessionState.ONGOING, SessionState.ENDED),
+                Arguments.of(SessionState.ENDED, SessionState.ENDED)
+        );
     }
 }
