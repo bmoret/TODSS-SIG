12c2b9eeb3290d4d750119660d650969c6851daa
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-04-07 12:09:39 +0200

    Add integration tests

--- src/test/java/com/snafu/todss/sig/sessies/application/SessionServiceIntegrationTest.java
+++ src/test/java/com/snafu/todss/sig/sessies/application/SessionServiceIntegrationTest.java
@@ -9,6 +9,9 @@ import com.snafu.todss.sig.sessies.domain.session.types.OnlineSession;
 import com.snafu.todss.sig.sessies.domain.session.types.PhysicalSession;
 import com.snafu.todss.sig.sessies.domain.session.types.Session;
 import com.snafu.todss.sig.sessies.domain.session.types.TeamsOnlineSession;
+import com.snafu.todss.sig.sessies.presentation.dto.request.session.OnlineSessionRequest;
+import com.snafu.todss.sig.sessies.presentation.dto.request.session.PhysicalSessionRequest;
+import com.snafu.todss.sig.sessies.presentation.dto.request.session.SessionRequest;
 import javassist.NotFoundException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,6 +26,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -106,5 +110,142 @@ class SessionServiceIntegrationTest {
                 NotFoundException.class,
                 () -> sessionService.getSessionById(UUID.randomUUID())
         );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCreateSessionArgs")
+    @DisplayName("Creating a session returns a newly made session")
+    void createSession_CreatesInstance(SessionRequest request, Class<Session> expectedClass) throws NotFoundException {
+        SpecialInterestGroup sig = sigRepository.save(new SpecialInterestGroup());
+        request.sigId = sig.getId();
+
+        Session session = sessionService.createSession(request);
+
+        assertTrue(expectedClass.isInstance(session));
+    }
+    private static Stream<Arguments> provideCreateSessionArgs() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime nowPlusOneHour = LocalDateTime.now().plusHours(1);
+        return Stream.of(
+                Arguments.of(
+                        new PhysicalSessionRequest(
+                                now,
+                                nowPlusOneHour,
+                                "Subject",
+                                "Description",
+                                UUID.randomUUID(),
+                                "Address"
+                        ),
+                        PhysicalSession.class
+                ),
+                Arguments.of(
+                        new OnlineSessionRequest(
+                                now,
+                                nowPlusOneHour,
+                                "Subject",
+                                "Description",
+                                UUID.randomUUID(),
+                                "Random Platform",
+                                "link"
+                        ),
+                        OnlineSession.class
+                ),
+                Arguments.of(
+                        new OnlineSessionRequest(
+                                now, nowPlusOneHour,
+                                "Subject",
+                                "Description",
+                                UUID.randomUUID(),
+                                "Teams",
+                                "link"
+                        ),
+                        TeamsOnlineSession.class
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCreateSessionArgs")
+    @DisplayName("Creating a session does not throw")
+    void createSession_DoesNotThrow(SessionRequest request){
+        SpecialInterestGroup sig = sigRepository.save(new SpecialInterestGroup());
+        request.sigId = sig.getId();
+
+        assertDoesNotThrow(() -> sessionService.createSession(request));
+    }
+
+    @Test
+    @DisplayName("Creating session with not existing special interest group throws")
+    void createSessionWithNotExistingSig_ThrowsNotFound() {
+        PhysicalSessionRequest request = new PhysicalSessionRequest(
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1),
+                "Subject",
+                "Description",
+                UUID.randomUUID(),
+                "Address"
+        );
+
+        assertThrows(
+                NotFoundException.class,
+                () -> sessionService.createSession(request)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCreateSessionArgs")
+    @DisplayName("Updating a session does not throw")
+    void updateSession_DoesNotThrow(SessionRequest request) throws NotFoundException {
+        SpecialInterestGroup sig = sigRepository.save(new SpecialInterestGroup());
+        request.sigId = sig.getId();
+        Session session = sessionService.createSession(request);
+        request.subject = "New Subject";
+
+        assertDoesNotThrow(() -> sessionService.updateSession(session.getId(), request));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCreateSessionArgs")
+    @DisplayName("Updating a session returns the updated session")
+    void updateSession_CreatesInstance(SessionRequest request) throws NotFoundException {
+        SpecialInterestGroup sig = sigRepository.save(new SpecialInterestGroup());
+        request.sigId = sig.getId();
+        Session session = sessionService.createSession(request);
+        request.subject = "New Subject";
+
+        session = sessionService.updateSession(session.getId(), request);
+
+        assertEquals(request.subject, session.getDetails().getSubject());
+    }
+
+    @Test
+    @DisplayName("Updating session with not existing special interest group throws")
+    void updateSessionWithNotExistingSig_ThrowsNotFound() {
+        PhysicalSessionRequest request = new PhysicalSessionRequest(
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1),
+                "Subject",
+                "Description",
+                UUID.randomUUID(),
+                "Address"
+        );
+
+        assertThrows(
+                NotFoundException.class,
+                () -> sessionService.updateSession(testSession.getId(), request)
+        );
+    }
+
+    @Test
+    @DisplayName("Deleting session deletes session")
+    void deleteSession_DeletesSession() {
+        sessionService.deleteSession(testSession.getId());
+        assertEquals(Collections.emptyList(), repository.findAll());
+    }
+
+    @Test
+    @DisplayName("Deleting session does not throw")
+    void deleteSession_DoesNotThrow() {
+        assertDoesNotThrow(() -> sessionService.deleteSession(testSession.getId()));
     }
 }
