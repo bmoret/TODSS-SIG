cb729d65f44b505a293da3588935b5843a8d23e1
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-04-08 12:48:00 +0200

    Remove unnecessary constructor

--- src/main/java/com/snafu/todss/sig/sessies/util/InputValidations.java
+++ src/main/java/com/snafu/todss/sig/sessies/util/InputValidations.java
@@ -4,9 +4,6 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class InputValidations {
-    private InputValidations() {
-    }
-
     public static void inputNotNull(Object... inputs) {
         Arrays.stream(inputs)
                 .forEach(input -> {
