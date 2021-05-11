b8ab1c0ead4e0f9e4468d93ce209237d2d52c999
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-05-11 15:58:17 +0200

    Add dummy data fix dto mapping Add CORS

--- src/main/java/com/snafu/todss/sig/sessies/presentation/dto/response/PersonCompactResponse.java
+++ src/main/java/com/snafu/todss/sig/sessies/presentation/dto/response/PersonCompactResponse.java
@@ -3,8 +3,11 @@ package com.snafu.todss.sig.sessies.presentation.dto.response;
 import java.util.UUID;
 
 public class PersonCompactResponse {
-    private final UUID personId;
-    private final String personName;
+    private UUID personId;
+    private String personName;
+
+    public PersonCompactResponse() {
+    }
 
     public PersonCompactResponse(UUID personId, String personName) {
         this.personId = personId;
@@ -15,7 +18,15 @@ public class PersonCompactResponse {
         return personId;
     }
 
+    public void setPersonId(UUID personId) {
+        this.personId = personId;
+    }
+
     public String getPersonName() {
         return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
     }
 }
