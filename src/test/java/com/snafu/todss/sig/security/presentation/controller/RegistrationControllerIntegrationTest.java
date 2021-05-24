d467dc162fb79b8f7ed125ba773966e6939ab39a
2m201 <melaniekelley@hotmail.com>
2021-05-24 14:01:36 +0200

    Fixed RegistrationControllerIntegrationTest

--- src/test/java/com/snafu/todss/sig/security/presentation/controller/RegistrationControllerIntegrationTest.java
+++ src/test/java/com/snafu/todss/sig/security/presentation/controller/RegistrationControllerIntegrationTest.java
@@ -1,21 +1,13 @@
 package com.snafu.todss.sig.security.presentation.controller;
 
 import com.snafu.todss.sig.security.data.SpringUserRepository;
-import com.snafu.todss.sig.security.domain.User;
-import com.snafu.todss.sig.sessies.application.PersonService;
-import com.snafu.todss.sig.sessies.data.SpringPersonRepository;
-import com.snafu.todss.sig.sessies.domain.person.Person;
-import com.snafu.todss.sig.sessies.presentation.dto.request.PersonRequest;
-import javassist.NotFoundException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -28,37 +20,11 @@ class RegistrationIntegrationControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private PersonService personService;
-
-    @Autowired
     private SpringUserRepository userRepository;
-
-    @Autowired
-    private SpringPersonRepository personRepository;
-
-    @BeforeEach
-    void setUp() throws NotFoundException {
-        PersonRequest dtoSupervisor = new PersonRequest();
-        dtoSupervisor.email = "test2@email.com";
-        dtoSupervisor.firstname = "Test";
-        dtoSupervisor.lastname = "Person";
-        dtoSupervisor.expertise = "none";
-        dtoSupervisor.branch = "VIANEN";
-        dtoSupervisor.role = "EMPLOYEE";
-        dtoSupervisor.employedSince = "01/01/2021";
-        dtoSupervisor.supervisorId = null;
-        Person supervisor = personService.createPerson(dtoSupervisor);
-        personRepository.save(supervisor);
-
-        User user = new User("TestUser", "TestPassword", supervisor);
-        userRepository.save(user);
-
-    }
 
     @AfterEach
     void tearDown() {
         this.userRepository.deleteAll();
-        this.personRepository.deleteAll();
     }
 
     @Test
@@ -91,7 +57,7 @@ class RegistrationIntegrationControllerTest {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("username", "TestUserName");
         jsonObject.put("password", "1234567");
-        jsonObject.put("email", "test2@email.com");
+        jsonObject.put("email", "test3@email.com");
         jsonObject.put("firstname", "FirstName");
         jsonObject.put("lastname", "LastName");
         jsonObject.put("expertise", "none");
@@ -115,7 +81,7 @@ class RegistrationIntegrationControllerTest {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("username", "TestUserName");
         jsonObject.put("password", "12345678");
-        jsonObject.put("email", "test2@email.com");
+        jsonObject.put("email", "test4@email.com");
         jsonObject.put("firstname", "FirstName");
         jsonObject.put("lastname", "LastName");
         jsonObject.put("expertise", "none");
