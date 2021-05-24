a9e567f09be470f109d55ca6c7659880a746cc71
Hvitson <Thomas_alberts@live.nl>
2021-05-24 16:00:55 +0200

    tijdelijke merge

--- src/test/java/com/snafu/todss/sig/sessies/presentation/controller/PersonControllerIntegrationTest.java
+++ src/test/java/com/snafu/todss/sig/sessies/presentation/controller/PersonControllerIntegrationTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class PersonControllerTest {
+class PersonControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -453,8 +453,8 @@ class PersonControllerTest {
     @WithMockUser(username = "TestUser", roles = "MANAGER")
     @DisplayName("Search person by name as manager")
     void searchPersonAsManager() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.post("/person/search")
-                .content("{\"firstname\":\"Test\"}")
+        RequestBuilder request = MockMvcRequestBuilders.get("/person/search")
+                .content("{\"searchTerm\":\"Test\"}")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
 
         mockMvc.perform(request)
@@ -468,8 +468,8 @@ class PersonControllerTest {
     @WithMockUser(username = "TestUser", roles = "SECRETARY")
     @DisplayName("Search person by name as secretary")
     void searchPersonAsSecretary() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.post("/person/search")
-                .content("{\"firstname\":\"Test\"}")
+        RequestBuilder request = MockMvcRequestBuilders.get("/person/search")
+                .content("{\"searchTerm\":\"Test\"}")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
 
         mockMvc.perform(request)
@@ -483,8 +483,8 @@ class PersonControllerTest {
     @WithMockUser(username = "TestUser", roles = "ADMINISTRATOR")
     @DisplayName("Search person by name as administrator")
     void searchPersonAsAdministrator() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.post("/person/search")
-                .content("{\"firstname\":\"Test\"}")
+        RequestBuilder request = MockMvcRequestBuilders.get("/person/search")
+                .content("{\"searchTerm\":\"Test\"}")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
 
         mockMvc.perform(request)
