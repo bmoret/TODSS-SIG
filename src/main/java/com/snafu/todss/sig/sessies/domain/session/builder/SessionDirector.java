2c8f978e8751ae06a8c05128ec50119a40dc77f5
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-06-18 13:54:32 +0200

    Fix bug, can change dates after planned state Add tests

--- src/main/java/com/snafu/todss/sig/sessies/domain/session/builder/SessionDirector.java
+++ src/main/java/com/snafu/todss/sig/sessies/domain/session/builder/SessionDirector.java
@@ -3,12 +3,14 @@ package com.snafu.todss.sig.sessies.domain.session.builder;
 import com.snafu.todss.sig.sessies.domain.SpecialInterestGroup;
 import com.snafu.todss.sig.sessies.domain.person.Person;
 import com.snafu.todss.sig.sessies.domain.session.SessionDetails;
+import com.snafu.todss.sig.sessies.domain.session.SessionState;
 import com.snafu.todss.sig.sessies.domain.session.types.OnlineSession;
 import com.snafu.todss.sig.sessies.domain.session.types.PhysicalSession;
 import com.snafu.todss.sig.sessies.domain.session.types.Session;
 import com.snafu.todss.sig.sessies.presentation.dto.request.session.OnlineSessionRequest;
 import com.snafu.todss.sig.sessies.presentation.dto.request.session.PhysicalSessionRequest;
 import com.snafu.todss.sig.sessies.presentation.dto.request.session.SessionRequest;
+
 import static com.snafu.todss.sig.sessies.util.InputValidations.inputNotNull;
 
 public class SessionDirector {
@@ -52,12 +54,18 @@ public class SessionDirector {
 
     public static Session rebuild(Session session, SessionRequest request, SpecialInterestGroup sig, Person contactPerson) {
         inputNotNull(request);
+        if (!session.getState().equals(SessionState.DRAFT)
+                || !session.getState().equals(SessionState.TO_BE_PLANNED)) {
+            request.startDate = session.getDetails().getStartDate();
+            request.endDate = session.getDetails().getEndDate();
+        }
         if (PhysicalSessionRequest.class.isAssignableFrom(request.getClass())){
             Session physicalSession = buildPhysicalSession((PhysicalSessionRequest) request, sig, contactPerson);
             physicalSession.setId(session.getId());
             physicalSession.addAllAttendees(session.getAttendances());
             physicalSession.addAllFeedback(session.getFeedback());
-            return physicalSession;
+
+                return physicalSession;
         } else if (OnlineSessionRequest.class.isAssignableFrom(request.getClass())) {
             Session onlineSession = buildOnlineSession((OnlineSessionRequest) request, sig, contactPerson);
             onlineSession.setId(session.getId());
@@ -67,6 +75,8 @@ public class SessionDirector {
         }
         throw new IllegalArgumentException("Cannot recreate session");
     }
+
+
 
     public static Session update(Session session, SessionRequest request, SpecialInterestGroup sig, Person contactPerson) {
         if (PhysicalSessionRequest.class.isAssignableFrom(request.getClass())) {
@@ -83,30 +93,35 @@ public class SessionDirector {
         throw new IllegalArgumentException("Cannot update session");
     }
 
-    private static Session updatePhysicalSession(PhysicalSession session, PhysicalSessionRequest request, SpecialInterestGroup sig, Person contactPerson) {
+    private static Session updateSession(Session session, SessionRequest request, SpecialInterestGroup sig, Person contactPerson) {
         SessionDetails details = session.getDetails();
-        details.setStartDate(request.startDate);
-        details.setEndDate(request.endDate);
         details.setSubject(request.subject);
         details.setDescription(request.description);
         session.setSig(sig);
-        session.setAddress(request.address);
         session.setContactPerson(contactPerson);
+        if (!session.getState().equals(SessionState.DRAFT) && !session.getState().equals(SessionState.TO_BE_PLANNED)) {
+            request.startDate = session.getDetails().getStartDate();
+            request.endDate = session.getDetails().getEndDate();
+        } else {
+            details.setStartDate(request.startDate);
+            details.setEndDate(request.endDate);
+        }
+
+        return session;
+    }
+
+    private static Session updatePhysicalSession(PhysicalSession session, PhysicalSessionRequest request, SpecialInterestGroup sig, Person contactPerson) {
+        updateSession(session, request, sig, contactPerson);
+        session.setAddress(request.address);
         return session;
     }
 
     private static Session updateOnlineSession(OnlineSession session, OnlineSessionRequest request, SpecialInterestGroup sig, Person contactPerson) {
-        SessionDetails details = session.getDetails();
-        details.setStartDate(request.startDate);
-        details.setEndDate(request.endDate);
-        details.setSubject(request.subject);
-        details.setDescription(request.description);
-        session.setSig(sig);
+        updateSession(session, request, sig, contactPerson);
         session.setJoinUrl(request.joinUrl);
         if (!session.getPlatform().equalsIgnoreCase("Teams")) {
             session.setPlatform(request.platform);
         }
-        session.setContactPerson(contactPerson);
         return session;
     }
 }
