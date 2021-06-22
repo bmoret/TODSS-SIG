1a04d6afdf501fd5300b412cd07219a7c039d4cb
Annemae <a.b.a.vandehoef@gmail.com>
2021-06-22 14:24:58 +0200

    fixed role to manager

--- src/main/java/com/snafu/todss/sig/sessies/presentation/controller/SessionController.java
+++ src/main/java/com/snafu/todss/sig/sessies/presentation/controller/SessionController.java
@@ -55,7 +55,7 @@ SessionController {
         return new ResponseEntity<>(convertSessionListToResponse(sessions), HttpStatus.OK);
     }
 
-    @PermitAll
+    @RolesAllowed("ROLE_MANAGER")
     @GetMapping("/history/{personId}")
     public ResponseEntity<List<SessionResponse>> getHistorySessionsOfPerson(@PathVariable UUID personId) throws NotFoundException {
         List<Session> sessions = this.SERVICE.getHistorySessionsOfPerson(personId);
